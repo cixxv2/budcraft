6d8f3e3661e57085bd2ce7b9fceaab637ff5bd78
cixxv2 <161750876+cixxv2@users.noreply.github.com>
2024-02-29 17:25:19 +0000

    Commit

---
+++ src/main/java/net/cixx/budcraft/BudcraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cixx.budcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BudcraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

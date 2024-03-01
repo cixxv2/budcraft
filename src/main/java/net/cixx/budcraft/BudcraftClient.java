57f2fe9a98b150a708d55369e8bfeacecc4da5d0
cixxv2 <161750876+cixxv2@users.noreply.github.com>
2024-03-01 07:04:14 +0000

    QOL

---
+++ src/main/java/net/cixx/budcraft/BudcraftClient.java
@@ -0,0 +1,10 @@
+package net.cixx.budcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BudcraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
